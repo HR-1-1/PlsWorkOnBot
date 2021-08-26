d60d83dc3be012bc7e74d06c8007a5abf2618ba7
Harish R <76567644+HR-1-1@users.noreply.github.com>
2021-08-26 17:40:20 +0530

    Sketch to upload on ESP32

---
+++ GRID_SKETCH/GRID_SKETCH.ino
@@ -0,0 +1,210 @@
+#include<WiFi.h>
+
+#define UP 1
+#define DOWN 2
+#define LEFT 3
+#define RIGHT 4
+#define UP_LEFT 5
+#define UP_RIGHT 6
+#define DOWN_LEFT 7
+#define DOWN_RIGHT 8
+#define TURN_LEFT 9
+#define TURN_RIGHT 10
+#define STOP 0
+
+#define FRONT_RIGHT_MOTOR 0
+#define BACK_RIGHT_MOTOR 1
+#define FRONT_LEFT_MOTOR 2
+#define BACK_LEFT_MOTOR 3
+
+#define FORWARD 1
+#define BACKWARD -1
+
+
+const char* ssid = "LAPTOP-3OSR4EKG 2152"; //Enter your Laptop hotspot ssid
+const char* pass = "9730|p7L"; //Enter your password for the same
+
+const int port = 8090;
+const char* host = "192.168.137.1"; // Enter the IP address of the local machine
+
+
+struct MOTOR_PINS
+{
+  int pinIN1;
+  int pinIN2;    
+};
+
+std::vector<MOTOR_PINS> motorPins = 
+{
+  {16, 17},  //FRONT_RIGHT_MOTOR      
+  {18, 19},  //BACK_RIGHT_MOTOR
+  {26, 25},  //FRONT_LEFT_MOTOR
+  {14, 27},  //BACK_LEFT_MOTOR   
+};
+
+
+void rotateMotor(int motorNumber, int motorDirection)
+{
+  if (motorDirection == FORWARD)
+  {
+    digitalWrite(motorPins[motorNumber].pinIN1, HIGH);
+    digitalWrite(motorPins[motorNumber].pinIN2, LOW);    
+  }
+  else if (motorDirection == BACKWARD)
+  {
+    digitalWrite(motorPins[motorNumber].pinIN1, LOW);
+    digitalWrite(motorPins[motorNumber].pinIN2, HIGH);     
+  }
+  else
+  {
+    digitalWrite(motorPins[motorNumber].pinIN1, LOW);
+    digitalWrite(motorPins[motorNumber].pinIN2, LOW);       
+  }
+}
+
+void processCarMovement(String inputValue)
+{
+  Serial.println("Got value as"); 
+  Serial.println(inputValue.c_str()); 
+  Serial.println(inputValue.toInt());
+  switch(inputValue.toInt())
+  {
+
+    case UP:
+      rotateMotor(FRONT_RIGHT_MOTOR, FORWARD);
+      rotateMotor(BACK_RIGHT_MOTOR, FORWARD);
+      rotateMotor(FRONT_LEFT_MOTOR, FORWARD);
+      rotateMotor(BACK_LEFT_MOTOR, FORWARD);                  
+      break;
+
+    case DOWN:
+      rotateMotor(FRONT_RIGHT_MOTOR, BACKWARD);
+      rotateMotor(BACK_RIGHT_MOTOR, BACKWARD);
+      rotateMotor(FRONT_LEFT_MOTOR, BACKWARD);
+      rotateMotor(BACK_LEFT_MOTOR, BACKWARD);   
+      break;
+
+    case LEFT:
+      rotateMotor(FRONT_RIGHT_MOTOR, FORWARD);
+      rotateMotor(BACK_RIGHT_MOTOR, BACKWARD);
+      rotateMotor(FRONT_LEFT_MOTOR, BACKWARD);
+      rotateMotor(BACK_LEFT_MOTOR, FORWARD);   
+      break;
+
+    case RIGHT:
+      rotateMotor(FRONT_RIGHT_MOTOR, BACKWARD);
+      rotateMotor(BACK_RIGHT_MOTOR, FORWARD);
+      rotateMotor(FRONT_LEFT_MOTOR, FORWARD);
+      rotateMotor(BACK_LEFT_MOTOR, BACKWARD);  
+      break;
+
+    case UP_LEFT:
+      rotateMotor(FRONT_RIGHT_MOTOR, FORWARD);
+      rotateMotor(BACK_RIGHT_MOTOR, STOP);
+      rotateMotor(FRONT_LEFT_MOTOR, STOP);
+      rotateMotor(BACK_LEFT_MOTOR, FORWARD);  
+      break;
+
+    case UP_RIGHT:
+      rotateMotor(FRONT_RIGHT_MOTOR, STOP);
+      rotateMotor(BACK_RIGHT_MOTOR, FORWARD);
+      rotateMotor(FRONT_LEFT_MOTOR, FORWARD);
+      rotateMotor(BACK_LEFT_MOTOR, STOP);  
+      break;
+
+    case DOWN_LEFT:
+      rotateMotor(FRONT_RIGHT_MOTOR, STOP);
+      rotateMotor(BACK_RIGHT_MOTOR, BACKWARD);
+      rotateMotor(FRONT_LEFT_MOTOR, BACKWARD);
+      rotateMotor(BACK_LEFT_MOTOR, STOP);   
+      break;
+
+    case DOWN_RIGHT:
+      rotateMotor(FRONT_RIGHT_MOTOR, BACKWARD);
+      rotateMotor(BACK_RIGHT_MOTOR, STOP);
+      rotateMotor(FRONT_LEFT_MOTOR, STOP);
+      rotateMotor(BACK_LEFT_MOTOR, BACKWARD);   
+      break;
+
+    case TURN_LEFT:
+      rotateMotor(FRONT_RIGHT_MOTOR, FORWARD);
+      rotateMotor(BACK_RIGHT_MOTOR, FORWARD);
+      rotateMotor(FRONT_LEFT_MOTOR, BACKWARD);
+      rotateMotor(BACK_LEFT_MOTOR, BACKWARD);  
+      break;
+
+    case TURN_RIGHT:
+      rotateMotor(FRONT_RIGHT_MOTOR, BACKWARD);
+      rotateMotor(BACK_RIGHT_MOTOR, BACKWARD);
+      rotateMotor(FRONT_LEFT_MOTOR, FORWARD);
+      rotateMotor(BACK_LEFT_MOTOR, FORWARD);   
+      break;
+
+    case STOP:
+      rotateMotor(FRONT_RIGHT_MOTOR, STOP);
+      rotateMotor(BACK_RIGHT_MOTOR, STOP);
+      rotateMotor(FRONT_LEFT_MOTOR, STOP);
+      rotateMotor(BACK_LEFT_MOTOR, STOP);    
+      break;
+
+    default:
+      rotateMotor(FRONT_RIGHT_MOTOR, STOP);
+      rotateMotor(BACK_RIGHT_MOTOR, STOP);
+      rotateMotor(FRONT_LEFT_MOTOR, STOP);
+      rotateMotor(BACK_LEFT_MOTOR, STOP);    
+      break;
+  }
+}
+
+void setUpPinModes()
+{
+  for (int i = 0; i < motorPins.size(); i++)
+  {
+    pinMode(motorPins[i].pinIN1, OUTPUT);
+    pinMode(motorPins[i].pinIN2, OUTPUT);  
+    rotateMotor(i, STOP);  
+  }
+}
+
+
+void setup(void) 
+{
+  setUpPinModes();
+  Serial.begin(115200);
+  WiFi.begin(ssid, pass);
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.println("...");
+  }
+  Serial.println("WiFi connected with IP : ");
+  Serial.println(WiFi.localIP());
+   
+  processCarMovement("0");
+  Serial.print("Things are going good");
+}
+
+void loop() 
+{
+   
+  WiFiClient client;
+
+  if (!client.connect(host, port))
+  {
+    Serial.println("Connection to host failed");
+    delay(1000);
+    return;
+  }
+  Serial.println("Connection to the server successful!");
+  
+  while(client.connected())
+  {
+    while(client.available() > 0)
+    {
+      char c = client.read();
+      String command = String(c);
+      processCarMovement(command); 
+      Serial.println(command);
+    }
+  }
+}
