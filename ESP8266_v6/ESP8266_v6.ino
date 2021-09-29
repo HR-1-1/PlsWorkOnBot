de965375590ada544e6624fbaca29713845a83ba
HR-1-1 <harishrajesh2002@gmail.com>
2021-09-29 17:32:23 +0530

    Add Sketches

---
+++ ESP8266_v6/ESP8266_v6.ino
@@ -0,0 +1,186 @@
+#include <AFMotor.h>
+#include <Servo.h>
+Servo Smotor;
+
+int Input_Command;
+int tilt_up = 90;
+int tilt_down = 180;
+
+AF_DCMotor Gmotor1(1);
+AF_DCMotor Gmotor2(2);
+AF_DCMotor Gmotor3(3);
+AF_DCMotor Gmotor4(4);
+
+void setup() {
+
+  Serial.begin(115200);
+
+  Smotor.attach(9);
+  Smotor.write(tilt_down);
+
+  Gmotor1.setSpeed(200);
+  Gmotor1.run(RELEASE);
+
+  Gmotor2.setSpeed(200);
+  Gmotor2.run(RELEASE);
+
+  Gmotor3.setSpeed(200);
+  Gmotor3.run(RELEASE);
+
+  Gmotor4.setSpeed(200);
+  Gmotor4.run(RELEASE);
+}
+
+
+void loop() {
+
+  uint8_t i;
+  int x_speed=120;
+
+  if (Serial.available()) {
+
+    Input_Command = Serial.read();
+
+    switch (Input_Command)
+    {
+      case 49:
+
+        Gmotor1.setSpeed(x_speed);
+        Gmotor2.setSpeed(x_speed);
+        Gmotor3.setSpeed(x_speed);
+        Gmotor4.setSpeed(x_speed);
+        Gmotor1.run(FORWARD);
+        Gmotor2.run(FORWARD);
+        Gmotor3.run(FORWARD);
+        Gmotor4.run(FORWARD);
+        
+        break;
+
+      case 50:
+        Gmotor1.run(FORWARD);
+        Gmotor2.run(FORWARD);
+        Gmotor3.run(FORWARD);
+        Gmotor4.run(FORWARD);
+        for (i = 0; i < x_speed; i++) {
+          Gmotor1.setSpeed(i);
+          Gmotor2.setSpeed(i);
+          Gmotor3.setSpeed(i);
+          Gmotor4.setSpeed(i);
+          delay(10);
+        }
+        for (i = x_speed; i != 0; i--) {
+          Gmotor1.setSpeed(i);
+          Gmotor2.setSpeed(i);
+          Gmotor3.setSpeed(i);
+          Gmotor4.setSpeed(i);
+        }
+        
+        break;
+
+      case 51:
+        Gmotor1.setSpeed(x_speed);
+        Gmotor2.setSpeed(x_speed);
+        Gmotor3.setSpeed(x_speed);
+        Gmotor4.setSpeed(x_speed);
+        Gmotor1.run(BACKWARD);
+        Gmotor2.run(BACKWARD);
+        Gmotor3.run(BACKWARD);
+        Gmotor4.run(BACKWARD);
+        break;
+      
+
+      case 52:
+
+        Gmotor1.run(BACKWARD);
+        Gmotor2.run(BACKWARD);
+        Gmotor3.run(BACKWARD);
+        Gmotor4.run(BACKWARD);
+        for (i = 0; i < x_speed; i++) {
+          Gmotor1.setSpeed(i);
+          Gmotor2.setSpeed(i);
+          Gmotor3.setSpeed(i);
+          Gmotor4.setSpeed(i);
+          delay(10);
+        }
+        for (i = x_speed; i != 0; i--) {
+          Gmotor1.setSpeed(i);
+          Gmotor2.setSpeed(i);
+          Gmotor3.setSpeed(i);
+          Gmotor4.setSpeed(i);
+        }
+        
+        break;
+
+      case 53:
+        Gmotor1.setSpeed(x_speed);
+        Gmotor2.setSpeed(x_speed);
+        Gmotor3.setSpeed(x_speed);
+        Gmotor4.setSpeed(x_speed);
+        Gmotor1.run(BACKWARD);
+        Gmotor2.run(FORWARD);
+        Gmotor3.run(FORWARD);
+        Gmotor4.run(BACKWARD);
+        break;
+       
+      case 54:
+        Gmotor1.setSpeed(x_speed);
+        Gmotor2.setSpeed(x_speed);
+        Gmotor3.setSpeed(x_speed);
+        Gmotor4.setSpeed(x_speed);
+        Gmotor1.run(BACKWARD);
+        Gmotor2.run(FORWARD);
+        Gmotor3.run(FORWARD);
+        Gmotor4.run(BACKWARD);
+        
+        break;
+
+      case 55:
+        Gmotor1.setSpeed(x_speed);
+        Gmotor2.setSpeed(x_speed);
+        Gmotor3.setSpeed(x_speed);
+        Gmotor4.setSpeed(x_speed);
+        Gmotor1.run(FORWARD);
+        Gmotor2.run(BACKWARD);
+        Gmotor3.run(BACKWARD);
+        Gmotor4.run(FORWARD);
+        break;
+
+      case 56:
+        Gmotor1.setSpeed(x_speed);
+        Gmotor2.setSpeed(x_speed);
+        Gmotor3.setSpeed(x_speed);
+        Gmotor4.setSpeed(x_speed);
+        Gmotor1.run(FORWARD);
+        Gmotor2.run(BACKWARD);
+        Gmotor3.run(BACKWARD);
+        Gmotor4.run(FORWARD);
+        break;
+        
+
+      case 57:
+        Smotor.write(tilt_up);
+        delay(100);
+        break;
+
+      case 58:
+        Smotor.write(tilt_down);
+        delay(100);
+        break;
+
+      case 48:
+        Gmotor1.setSpeed(0);
+        Gmotor2.setSpeed(0);
+        Gmotor3.setSpeed(0);
+        Gmotor4.setSpeed(0);
+        Gmotor1.run(RELEASE);
+        Gmotor2.run(RELEASE);
+        Gmotor3.run(RELEASE);
+        Gmotor4.run(RELEASE);
+        delay(100);
+        break;
+
+
+
+    }
+  }
+}
---
+++ WIFI_v6/WIFI_v6.ino
@@ -0,0 +1,45 @@
+#include <ESP8266WiFi.h>        // Include the Wi-Fi library
+
+const char* ssid     = "LAPTOP-30SR4EKG 2152";
+// The SSID (name) of the Wi-Fi network you want to connect to
+const char* password = "9730|p7L";                        // The password of the Wi-Fi network
+
+const int port = 8094;
+const char* host = "192.168.137.1";
+
+void setup() {
+  Serial.begin(115200);         // Start the Serial communication with UNO
+  delay(10);
+
+  WiFi.mode(WIFI_STA);              //Multiple Connections
+  WiFi.begin(ssid, password);       // Connect to the network
+
+  int i = 0;
+  while (WiFi.status() != WL_CONNECTED) { // Wait for the Wi-Fi to connect
+    delay(1000);
+  }
+
+}
+
+
+void loop()
+{
+
+  WiFiClient client;
+
+  if (!client.connect(host, port))
+  {
+    delay(1000);
+    return;
+  }
+
+  while (client.connected())
+  {
+    while (client.available() > 0)
+    {
+      char c = client.read();
+      int command = int(c);
+      Serial.write(command);
+    }
+  }
+}
